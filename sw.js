8602c78bab47bb3564815706882edecd20926a64
ziupa86-arch <ziupa86@icloud.com>
2025-12-15 22:25:38 +0000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = "baza-aut-v1";
+const ASSETS = ["./", "./index.html", "./app.js", "./manifest.json"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k === CACHE ? null : caches.delete(k))))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request).catch(() => caches.match("./index.html")))
+  );
+});
